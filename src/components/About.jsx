6020739686d0afaef869544c127cd1404a2f891d
Reimei Rigel <sara_rigel@icloud.com>
2022-06-09 11:47:37 +0900

    about追加

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,28 @@ import React from 'react'
 const About = () => {
   return (
     <div className='aboutPie'>
-      製作中<br />
-      Coming soon…
+      <h1>About</h1>
+      <h3>展覧会の様子</h3>
+      <div className='aboutImg'>
+        <div>
+          <img src="about_6.jpeg" alt="" />
+        </div>
+        <div>
+          <img src="about_2.jpeg" alt="" />
+        </div>
+        <div>
+          <img src="about_3.jpeg" alt="" />
+        </div>
+        <div>
+          <img src="about_1.jpeg" alt="" />
+        </div>
+       <div>
+        <img src="about_4.jpeg" alt="" />
+       </div>
+        <div>
+          <img src="about_5.jpeg" alt="" />
+        </div>
+      </div>
     </div>
   )
 }
